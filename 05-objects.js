f0213235ecf0aa0094963182815afbb05cac107e
Kody Carling <kodycarling19@gmail.com>
2022-03-08 14:40:02 -0700

    finished objects

--- 05-objects.js
+++ 05-objects.js
@@ -7,9 +7,13 @@ const cookie = {
   price: 2.5,
   isGlutenFree: false,
   "+yummy": true,
+  eatCookie: function () {
+    console.log("I am eating a " + this.name);
+  },
 };
 
 console.log(cookie.name);
 cookie.name = "chip chocolate";
 console.log(cookie.name);
 console.log(cookie["+yummy"]);
+cookie.eatCookie();
--- 06-classes.js
+++ 06-classes.js
@@ -3,6 +3,11 @@ class Cookie {
     this.name = name;
     this.isGlutenFree = isGlutenFree;
   }
+  eatCookie() {
+    console.log("I am eating a " + this.name);
+  }
 }
 const myCookie = new Cookie("Iced Oatmeal", false);
+console.log(myCookie);
 console.log(myCookie.name);
+myCookie.eatCookie();
