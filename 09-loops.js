61a052313363b1023f002ded7a86dc919e37d2e6
Kody Carling <kodycarling19@gmail.com>
2022-03-10 14:05:11 -0700

    finished 8 start 9

---
+++ 09-loops.js
@@ -0,0 +1,12 @@
+// for loop
+const names = ["John", "Mark", "Jane", "Mary"];
+
+for (let name = 0; name < names.length; name++) {
+  console.log(names[name]);
+}
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let number = 0; number < numbers.length; number++) {
+  console.log(numbers[number] * numbers[number]);
+}
